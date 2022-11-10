289670e8f6a3f225b300cbc25549957189dc736a
eunicekinzengele <eunicekinzengele0@gmail.com>
2022-11-10 15:05:10 +0100

    sidebar menu adjustments

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,31 +4,30 @@ import Logout from "./Logout";
 import { Link } from "react-router-dom";
 
 function SideBar() {
-  const profil= localStorage.getItem("profilUser") 
+  const profil = localStorage.getItem("profilUser");
   return (
     <div className="div__dashboard">
       <div className="container_list">
-      <ul className="list__dashboard">
-        <li>
-          <Link 
-          to={"/ParentsCardPopular"} className="side_list">
-            Tous
-          </Link>
-        </li>
-        <li>
-          <Link to={"/ParentsCardChannel"} className="side_list">
-            Abonnements
-          </Link>
-        </li>
-        <li>
-          <li>
-            <img className="profil_img" src={profil}/>
+        <ul className="list__dashboard">
+          <li className="side_list">
+            <Link to={"/ParentsCardPopular"} >
+              Tous
+            </Link>
           </li>
-          <button className="btn_logout">
-            <Logout />
-          </button>
-        </li>
-      </ul>
+          <li className="side_list">
+            <Link to={"/ParentsCardChannel"} >
+              Abonnements
+            </Link>
+          </li>
+          <li>
+            <li>
+              <img className="profil_img" src={profil} />
+            </li>
+            <button className="btn_logout">
+              <Logout />
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   );
