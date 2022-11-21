4f579e0b174a61dd4d0226c66758dc83c1fd2ede
eunicekinzengele <eunicekinzengele0@gmail.com>
2022-11-21 15:30:06 +0100

    Create a link to the home page on the navbar logo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../Styles/navbar.css";
 import image from "../Images/logo_NiceTube.png";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 // import { useMediaQuery } from 'react-responsive'
 
 
@@ -11,7 +12,9 @@ function NavBar() {
 
   return (
     <div className="div__nav">
+     <Link to= "/PageOfPopularVideo">
      <img className="img__logo" src={image} />
+     </Link>
       <ul>
         <SearchInput />
       </ul>
