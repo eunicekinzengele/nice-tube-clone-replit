114e9a0ea8a6adc379898830fbf3ca3fff6010ef
eunicekinzengele <eunicekinzengele0@gmail.com>
2022-11-22 12:02:02 +0100

    adjust container size on login page

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,13 +3,8 @@ import "../Styles/navbar.css";
 import image from "../Images/logo_NiceTube.png";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
-// import { useMediaQuery } from 'react-responsive'
 
-
-function NavBar() {
-  // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-  // const isMobile = useMediaQuery({ query: '(max-width: 425px)' })
-
+function NavBar() { 
   return (
     <div className="div__nav">
      <Link to= "/PageOfPopularVideo">
