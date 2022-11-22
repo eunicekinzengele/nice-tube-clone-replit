d83e364f214241cb558bfa5894896763bb20fbea
eunicekinzengele <eunicekinzengele0@gmail.com>
2022-11-22 12:21:23 +0100

    adjust container size on login page

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() { 
   return (
+    
     <div className="div__nav">
      <Link to= "/PageOfPopularVideo">
      <img className="img__logo" src={image} />
