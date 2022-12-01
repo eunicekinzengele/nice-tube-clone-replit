1ea1069c2f70986a1dc1169eac4618116e56db5a
eunicekinzengele <eunicekinzengele0@gmail.com>
2022-12-01 16:21:28 +0100

    feat: create profile page, photo and social media inputs

--- src/Components/Profil.jsx
+++ src/Components/Profil.jsx
@@ -41,9 +41,9 @@ export default function profil() {
           </div>
 
           <div class="container col-8 col-auto card cont_profil">
-            <div className="container">
-              <div className="profil_page flex-wrap d-flex justify-content-center">
-                <div className="row profil_image justify-content-center">
+            <div className="container ">
+              <div className="row profil_page flex-wrap d-flex justify-content-center">
+                <div className="row profil_image d-flex flex-wrap justify-content-center">
                   <h2 className="profil_title">Modifiez votre profil</h2>
                   <div className="row img_and_icon d-flex flex-wrap">
                     <img className="img_profil" src={Image}></img>
@@ -51,6 +51,40 @@ export default function profil() {
                       <img className="appareil_photo" src={Appareil}></img>
                     </button>
                   </div>
+                </div>
+                <div className="div_profilname">
+                  <input
+                    className="input_profil_name"
+                    placeholder="Modifiez votre nom"
+                    type="texte"
+                  />
+                  <button className="button_modifier" type="submit">
+                    {/* <BsSearch /> */}
+                  </button>
+                </div>
+              </div>
+              <div className=" div_liens container text-align-center ">
+                <h2 className="profil_title">Liens réseaux sociaux</h2>
+                <div className="row row_liens flex-wrap d-flex">
+                  <div className="container container_link">
+                    <div className="col facebook lien_rx">
+                      <input className="link inputfacebooklink" placeholder="Ajoutez votre lien Facebook" type="texte" />
+                    </div>
+                    <div className="col instagram lien_rx">
+                      <input className="link" placeholder="Ajoutez votre lien Instagram" type="texte" />
+                    </div>
+                    <div className="col tiktok lien_rx">
+                      <input className="link" placeholder="Ajoutez votre lien Tiktok" type="texte" />
+                    </div>
+                    <div className="col twitter lien_rx">
+                      <input className="link" placeholder="Ajoutez votre lien Twitter" type="texte" />
+                    </div>
+                  </div>
+                </div>
+                <div className="container buttons_annuler_modifier">
+                    <button className="col modifier_annuler">Modifier</button>
+                    <button className="col modifier_annuler">Annuler</button>
+
                 </div>
               </div>
             </div>
