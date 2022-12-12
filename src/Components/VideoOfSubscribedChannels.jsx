ffd0fe73f3d6013ebc92cd5668e633e96dee7546
eunicekinzengele <eunicekinzengele0@gmail.com>
2022-12-12 14:39:00 +0100

    feat: insertion of the sidebar component in the page of videos of subscribed channels

--- src/Components/VideoOfSubscribedChannels.jsx
+++ src/Components/VideoOfSubscribedChannels.jsx
@@ -5,6 +5,7 @@ import { contexteUse } from "./Contextes/UseContexte";
 import { useContext } from "react";
 import Logout from "./Logout";
 import Loader from "./Loader";
+import Sidebar from "./Sidebar";
 
 export default function VideoOfSubscribedChannels() {
   const [loading, setLoading] = useState(true);
@@ -30,34 +31,7 @@ export default function VideoOfSubscribedChannels() {
       <div class="row">
         <div className="container col-3 vh-100"></div>
         <div class="container col-3 bg-dark card vh-100 cont_left align-items-center">
-          <div className="side_link align-items-center">
-            <NavLink
-              className={(nav) =>
-                nav.isActive ? "side_list_red" : "side_list "
-              }
-              to={"/PageOfPopularVideo"}
-            >
-              Accueil
-            </NavLink>
-          </div>
-          <div className="side_link align-items-center">
-            <NavLink
-              className={(nav) =>
-                nav.isActive ? "side_list_red" : "side_list "
-              }
-              to={"/PageOfSubscribedChannels"}
-            >
-              Abonnements
-            </NavLink>
-          </div>
-          <div className="img_buton">
-            <Link to={`/PageOfProfil`}>
-              <img className="profil_img" src={profil} />
-            </Link>
-            <button className="btn btn_logout">
-              <Logout />
-            </button>
-          </div>
+        <Sidebar />
         </div>
 
         <div class="container col-8 col-auto bg-dark card cont-rigth">
