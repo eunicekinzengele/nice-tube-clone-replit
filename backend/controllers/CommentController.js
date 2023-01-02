6ceb2b5c5bfa178dded1d16086a11d2232b9c006
eunicekinzengele <eunicekinzengele0@gmail.com>
2023-01-02 14:14:47 +0100

    first request for creating or adding comments

--- backend/controllers/CommentController.js
+++ backend/controllers/CommentController.js
@@ -1,2 +1,14 @@
-const Comment = require('../models/CommentModel');
+const { NewLineKind } = require("typescript");
+const { default: Comment } = require("../../src/Components/Comment");
+const Comment = require("../models/CommentModel");
+
+exports.createAComment = (req, res, next) => {
+  const comment = new Comment({
+    ...req.body,
+  });
+  comment
+    .save()
+    .then(() => res.status(201).json({ message: "new comment created" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
