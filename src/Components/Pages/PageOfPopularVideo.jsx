98d03985194ac92e25a90ff51b6c46dc8a3b6437
eunicekinzengele <eunicekinzengele0@gmail.com>
2023-01-13 10:20:47 +0100

    responsive of the body of the page

--- src/Components/Pages/PageOfPopularVideo.jsx
+++ src/Components/Pages/PageOfPopularVideo.jsx
@@ -3,6 +3,7 @@ import NavBar from "../NavBar";
 import PopularVideosCard from "../PopularVideosCard";
 
 export default function PageOfPopularVideo() {
+
   return (
     <div className="exemple">
       <NavBar />
