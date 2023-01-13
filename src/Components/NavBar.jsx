8322e75cd1b70cd2e461315ab3ea5e24f2316262
eunicekinzengele <eunicekinzengele0@gmail.com>
2023-01-13 09:40:07 +0100

    responsive of the navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,24 +4,27 @@ import image from "../Images/logo_NiceTube.png";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
 // import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Col from "react-bootstrap/Col";
 
+function NavBar() {
+  const profil = localStorage.getItem("profilUser");
 
-function NavBar() { 
   return (
     <section className="header fixed-top container text-center text-light">
-    <div className="row">
+      <div className="row">
         <Col className="logo_accueil">
-          <Link to= "/PageOfPopularVideo">
-          <img className="img__logo" src={image} />
+          <Link to="/PageOfPopularVideo">
+            <img className="img__logo" src={image} />
           </Link>
         </Col>
         <Col>
-           <SearchInput />
+          <SearchInput />
         </Col>
-      <ul>
-      </ul>
-    </div>
+        <Col className="profilPhoto_nav">
+          <img className="profil_img_nav" src={profil} />
+        </Col>
+        <ul></ul>
+      </div>
     </section>
   );
 }
